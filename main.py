7829b12723cce821fbe93feafc06a7977b4c90b5
ksmyshlyaev <smyshlyaevkonst@gmail.com>
2023-04-29 21:43:30 +0400

    Added new value for sorting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-j = [1, 2, 3, 4]
+j = [1, 2, 3, 4, 5]
 
 
 def print_hi(name):
