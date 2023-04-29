10fc4b85e895760587fd4d24cd6d5150d916df92
ksmyshlyaev <smyshlyaevkonst@gmail.com>
2023-04-29 23:12:12 +0400

    Added isort to the project

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import os
+from sys import exit
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-j = [1, 2, 3, 4, 5]
+j = [1, 2, 3, 4, 5, 6]
 
 
 def print_hi(name):
