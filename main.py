622f74e48baa4d15d61e04fac75cce4e857a97ca
ksmyshlyaev <smyshlyaevkonst@gmail.com>
2023-04-29 21:42:22 +0400

    Reformatted file

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-j = [1,
-     2,
-     3]
+j = [1, 2, 3, 4]
 
 
 def print_hi(name):
